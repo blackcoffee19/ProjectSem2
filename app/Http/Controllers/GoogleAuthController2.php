47a8db1de9ef43d483f0904c65f5c14f098364dd
blackcoffee19 <cattuongw2000@gmail.com>
2023-04-25 08:55:47 +0700

    25/04 Tuong commit

---
+++ app/Http/Controllers/GoogleAuthController2.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Carbon\Carbon;
+use App\Models\Cart;
+use App\Models\News;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\File;
+
+class GoogleAuthController extends Controller
+{
+    public function redirect(){
+        return Socialite::driver('google')->redirect();
+    }
+    public function callbackGoogle(){
+        try{
+            $google_user = Socialite::driver('google')->stateless()->user();
+            $user = User::where('google_id','=',$google_user->getId())->orWhere('email','=',$google_user->getEmail())->first();
+            if(!$user){
+                $new_user = new User();
+                $new_user->name = $google_user->getName();
+                $new_user->email =$google_user->getEmail();
+                $new_user->google_id = $google_user->getId();
+                if($google_user->getAvatar()){
+                    $file = file_get_contents($google_user->getAvatar());
+                    File::put("images/avatar/gguser_".$google_user->getId().".jpg",$file);
+                    $new_user->avatar = "gguser_".$google_user->getId().".jpg";
+                }
+                $new_user->email_verified_at = Carbon::now()->format('Y-m-d H:i:s');
+                $new_user->created_at = Carbon::now()->format('Y-m-d H:i:s');
+                $new_user->save();
+                if(Session::has("cart")){
+                    $cart_session = Session::get("cart");
+                    $user = User::where('google_id','=',$google_user->getId())->first();
+                    foreach($cart_session as $key => $value){
+                        $addToUserCart = new Cart();
+                        $addToUserCart->id_user = $user->id_user;
+                        $addToUserCart->id_product = $value["id_product"];
+                        $addToUserCart->amount = $value["amount"];
+                        $addToUserCart->price = $value['per_price'];
+                        $addToUserCart->sale = $value['sale'];
+                        $addToUserCart->created_at = Carbon::now()->format('Y-m-d H:i:s');
+                        $addToUserCart->save();
+                    }
+                    Session::forget("cart");
+                };
+                $news = new News();
+                $news->title = "Please take the last step to activate account";
+                $news->link = "accountsetting";
+                $news->created_at = Carbon::now()->format('Y-m-d H:i:s');
+                $news->id_user = $new_user->id_user;
+                $news->save();
+                Auth::login($new_user);
+                return redirect('/');
+            }else{
+                if(Session::has("cart")){
+                    $cart_session = Session::get("cart");
+                    foreach($cart_session as $key => $value){
+                        $foundPro = $user->Cart->where('id_product','=',$value["id_product"])->first();
+                        if($foundPro != null){
+                            if(($foundPro->amount + $value["amount"]) >= $value['max']){
+                                $foundPro->amount = $user->Cart->where('id_product','=',$value["id_product"])->first()->Product->quantity;
+                            }else{
+                                $foundPro->amountt += $value["amount"];
+                            };
+                            $foundPro->price = $value['per_price'];
+                            $foundPro->sale = $value['sale'];
+                            $foundPro->save();
+                        }else{
+                            $addToUserCart = new Cart();
+                            $addToUserCart->id_user = $user->id_user;
+                            $addToUserCart->id_product = $value["id_product"];
+                            $addToUserCart->amount = $value["amount"];
+                            $addToUserCart->price = $value['per_price'];
+                            $addToUserCart->sale = $value['sale'];
+                            $addToUserCart->created_at = Carbon::now()->format('Y-m-d H:i:s');
+                            $addToUserCart->save();
+                        }
+                    }
+                    Session::forget("cart");
+                };
+                Auth::login($user);
+                return redirect('/');
+            }
+        }catch(\Throwable $th){
+            dd($th);
+        }
+    }
+}
